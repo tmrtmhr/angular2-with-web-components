7d6ef79e55b46af37af2cc9a95bd490745228c46
TAMURA Tomohiro <osone3@gmail.com>
2016-02-22 14:54:20 +0900

    google-map-dragend イベントをハンドリング

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,7 +13,7 @@ import {GoogleMapWebComponent} from './googlemap.component'
     height: 600px;
   }
 </style>
-<google-map [latitude]="lat" [longitude]="lng"></google-map>`
+<google-map latitude="{{lat}}" [longitude]="lng" [dragEvents]="true" (google-map-dragend)="dragendHandler($event)" (google-map-click)="clickHandler($event)"></google-map>`
 })
 export class AppComponent {
     @ViewChild(GoogleMapWebComponent)
@@ -31,11 +31,17 @@ export class AppComponent {
     }
 
     goWest() {
-        this.lng = this.lng - 0.1; // Angular 側のプロパティ更新が Web Components に反映される
+        this.lat = this.lat - 0.1; // Angular 側のプロパティ更新が Web Components に反映される
     }
 
     print() {
         console.log('[Angular2 Component Property] Lat: ', this.lat, ' Lng: ', this.lng);
         this.googlemap.printLatLng(); // 子コンポーネントのメソッド呼び出し
+    }
+
+    dragendHandler(event) {
+        console.log('catch google-map-dragend');
+        this.lat = event.target.latitude;
+        this.lng = event.target.longitude;
     }
 }
